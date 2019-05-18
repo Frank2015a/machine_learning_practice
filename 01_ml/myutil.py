3063c8292aa56cd989d5d272a07b8a4d221132ee
cheng.ma <frank.2015@qq.com>
2019-05-18 20:46:27 +0800

    0518

--- 01_ml/myutil.py
+++ 01_ml/myutil.py
@@ -1,39 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# this is my util
-
-
-# In[9]:
-
-
 from contextlib import contextmanager
 import time
 import json
 import requests
 import socket
 import sys
-import os
-
-
-# In[14]:
-
-
-os.system('jupyter nbconvert --to script myutil.ipynb')
-
-
-# In[12]:
-
-
-get_ipython().system('jupyter nbconvert\u200a--to script util.ipynb')
-
-
-# # my context
-
-# In[3]:
 
 
 @contextmanager
@@ -42,10 +12,6 @@ def gc2():
     yield
     gc.collect()
 
-
-# In[4]:
-
-
 @contextmanager
 def timer(title='do'):
     t0 = time.time()
@@ -53,15 +19,10 @@ def timer(title='do'):
     print("{} - done in {:.4f}s".format(title, time.time() - t0))
 
 
-# # messange
-
-# In[5]:
-
-
-token = "130fe712252513ab163f9453dbe9bdc9efca01eb3a7f1def85a5f6d89dc7c358"
-phones = [17621684529]
 # def bot(message, phones=phones, token=token):
 def ding(message):
+    token = "130fe712252513ab163f9453dbe9bdc9efca01eb3a7f1def85a5f6d89dc7c358"
+    phones = [17621684529]
     url = 'https://oapi.dingtalk.com/robot/send'
     params = { 'access_token': token }
     headers = { 'Content-Type': 'application/json' }
@@ -76,14 +37,49 @@ def ding(message):
     return requests.post(url, headers=headers, params=params, data=data)
 
 
-# In[7]:
+
+def convert_types(df, print_info = False):
+    original_memory = df.memory_usage().sum()
+    # Iterate through each column
+    for c in df:        
+        # Convert ids and booleans to integers
+        if ('SK_ID' in c):
+            df[c] = df[c].fillna(0).astype(np.int32)            
+        # Convert objects to category
+        elif (df[c].dtype == 'object') and (df[c].nunique() < df.shape[0]):
+            df[c] = df[c].astype('category')        
+        # Booleans mapped to integers
+        elif list(df[c].unique()) == [1, 0]:
+            df[c] = df[c].astype(bool)        
+        # Float64 to float32
+        elif df[c].dtype == float:
+            df[c] = df[c].astype(np.float32)            
+        # Int64 to int32
+        elif df[c].dtype == int:
+            df[c] = df[c].astype(np.int32)
+        
+    new_memory = df.memory_usage().sum()    
+    if print_info:
+        print(f'Original Memory Usage: {round(original_memory / 1e9, 2)} gb.')
+        print(f'New Memory Usage: {round(new_memory / 1e9, 2)} gb.')
+    return df
 
 
-# ding('ok')
+def df_null_stat(df, thres_null=0.5):
+    """
+    args:
+        - thres_null, thres to filter cols to drop
+    return:
+        - df_null, df of null ratios 
+    useage:
+    df_null, col_drop = df_null_stat(df, 0.5)
+    """ 
+    df_null = pd.DataFrame(df.isnull().sum() / df.shape[0])
+    df_null.rename(columns={0:'null_ratio'}, inplace=True);
+    df_null = df_null.sort_values(by='null_ratio', ascending=False)
+    
+    num_null = df_null[df_null.null_ratio > thres_null].shape[0]
+    print('num of col null ratio>{}: {}'.format(thres_null, num_null))
+    cols_drop = list(df_null[df_null.null_ratio > thres_null].index)
 
-
-# In[ ]:
-
-
-
-
+    return df_null, cols_drop
